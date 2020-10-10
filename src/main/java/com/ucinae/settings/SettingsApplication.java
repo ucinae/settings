f9d40ee60a06c678996fc56162b9900abff930d6
hansol <onesorry49@gmail.com>
2020-10-10 17:53:55 +0900

    init

---
+++ src/main/java/com/ucinae/settings/SettingsApplication.java
@@ -0,0 +1,13 @@
+package com.ucinae.settings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ucinae/settings/SettingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucinae.settings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
